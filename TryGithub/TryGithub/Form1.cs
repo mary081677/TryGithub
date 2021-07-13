0b7f2e8663e36c949e7340cafaccd0431a270538
秋 aki <mary0816773240@gmail.com>
2021-07-13 23:53:11 +0800

    【開發】加入按鍵，跳警告訊息

--- TryGithub/TryGithub/Form1.cs
+++ TryGithub/TryGithub/Form1.cs
@@ -17,9 +17,10 @@ namespace TryGithub
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
         }
     }
 }
